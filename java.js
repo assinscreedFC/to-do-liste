faf07ecf56960af776015d89e609a32e3c0d62b9
anis <anishammouche50@gmail.com>
2024-05-03 22:47:33 +0100

    toudoom

--- java.js
+++ java.js
@@ -9,10 +9,6 @@ modalTriggers.forEach((trigger) =>
   trigger.addEventListener("click", () => togglemodal())
 );
 
-function togglemodal() {
-  modalContainer.classList.toggle("active");
-}
-
 class note {
   constructor(titre, desc, dateg, rad) {
     this.Titre = titre;
@@ -31,19 +27,45 @@ class note {
     neww.querySelector(".para").innerHTML = this.Titre;
     // neww.querySelector("#description").innerHTML = this.Description;
     neww.querySelector(".date").value = this.date;
+    let couleur;
+    if (radio[0].checked) {
+      couleur = "#00FF00";
+    } else if (radio[1].checked) {
+      couleur = "#FFA500";
+    } else {
+      couleur = "#FF0000";
+    }
+
+    neww.querySelector(".ligne").style.borderLeft = " 4px solid " + couleur;
+    // neww.querySelector("#details").classList.add("addbook");
+    // neww.querySelector("#details").classList.add("modal-btn");
+    // neww.querySelector("#details").classList.add("modal-trigger");
     // neww.querySelector(".rad").innerHTML = this.rad;
     console.log(neww);
+
+    this.addbtn(neww);
+  }
+
+  addbtn(neww) {
+    const btn = neww.querySelector(".modal-trigger");
+
+    btn.addEventListener("click", () => togglemodal());
   }
 }
 
 const form = document.getElementById("form");
-const submit = document.getElementById("details");
+const submit = document.getElementById("add");
 submit.addEventListener("submit", (e) => e.preventDefault());
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  togglemodal();
   // radio = radio.filter((r) => r.checked);
-  console.log(radio[0].value);
+  console.log(radio[0]);
   console.log(date.value);
-  anis = new note(titre.value, description.value, date.value, radio[0].checked);
+  anis = new note(titre.value, description.value, date.value, radio);
   anis.setNote();
 });
+
+function togglemodal() {
+  modalContainer.classList.toggle("active");
+}
